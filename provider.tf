41e6ef00f16dca439a30da1cd4b71c9bcc931a42
chinazam C. Dennar <exchangehub@aol.com>
2023-11-19 18:31:02 -0700

    final commit

--- provider.tf
+++ provider.tf
@@ -4,10 +4,20 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.6.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.6.0"
+    }
   }
 }
 
 provider "google" {
+  credentials = file("my-key.json")
+  project     = var.google_cloud_project_id
+  region      = "us-cental1"
+}
+
+provider "google-beta" {
   credentials = file("my-key.json")
   project     = var.google_cloud_project_id
   region      = "us-cental1"
